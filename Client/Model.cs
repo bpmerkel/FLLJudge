99c820fc8318c707f9c3107aee6f489ed90a4c27
Brady P. Merkel <brady@merkel.net>
2023-11-11 23:32:49 -0500

    read json

---
+++ Client/Model.cs
@@ -0,0 +1,30 @@
+﻿using BlazorApp.Shared;
+using System.Text.Json;
+
+namespace BlazorApp.Client;
+
+public class Model
+{
+    public List<Area>? areas {  get; set; }
+    
+    public static Model GetModel()
+    {
+        var file = "comments.json";
+        // deserialize the json file
+        var json = File.ReadAllText(file);
+        return JsonSerializer.Deserialize<Model>(json);
+    }
+}
+
+public class Area
+{
+    public required string name { get; set; }
+    public List<Comment>? comments { get; set; }
+}
+
+public class Comment
+{
+    public int key { get; set; }
+    public required string text { get; set; }
+    public int rating { get; set; }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -8,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(s => Model.GetModel());
+
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
