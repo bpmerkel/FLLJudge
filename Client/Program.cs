cc2c4cff5ac0cfd0ff856dfcfd8fb20a91c00e85
Brady P. Merkel <brady@merkel.net>
2023-11-12 00:59:41 -0500

    fetch json filed from web host

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddSingleton(s =>
 {
-    var httpClient = s.GetRequiredService<HttpClient>();
+    using var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
     return Model.GetModel(httpClient);
 });
 
