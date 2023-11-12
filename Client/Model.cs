2e77f5752a466fddbfcd5d9cebdbf34dc26eab04
Brady P. Merkel <brady@merkel.net>
2023-11-12 18:09:32 -0500

    Vastly improved; no errors

--- Client/Model.cs
+++ Client/Model.cs
@@ -1,52 +1,74 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
+using System.Net.Http.Json;
 
 namespace BlazorApp.Client;
 // see https://stackoverflow.com/questions/58523617/blazor-client-side-webassembly-reading-a-json-file-on-startup-cs
 public class Model
 {
     [JsonPropertyName("areas")]
-    public List<Area>? Areas { get; set; }
+    public List<Area> Areas { get; set; }
 
-    public async static Task<Model> GetModel(HttpClient httpClient)
+    public async static Task<Model> GetModel(string baseAddress)
     {
-        var json = await httpClient.GetStringAsync("comments.json")
+        Console.WriteLine($"getting model from {baseAddress}");
+        using var httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
+        var model = await httpClient.GetFromJsonAsync<Model>("comments.json")
             .ConfigureAwait(false);
-        var model = JsonSerializer.Deserialize<Model>(json);
+        Console.WriteLine(model);
         foreach (var area in model.Areas)
         {
-            if (area == null) continue;
-            area.Chips = area.Comments
-                .SelectMany(c =>
-                {
-                    return c.Text.Split(" \t;.:,".ToCharArray())
-                        .Where(w => w.Length > 3)
-                        .GroupBy(w => w)
-                        .Where(g => g.Count() < 7)
-                        .SelectMany(g => g);
-                })
+            // set the tags from the text
+            area.Tags = area.Comments
+                .SelectMany(c => c.Text.Split(" \t;.:,".ToCharArray()))
+                .Where(w => w.Length > 4)
+                .Select(w => w.ToLower())
+                .GroupBy(w => w)
+                .Select(g => new { g.Key, count = g.Count() })
+                .OrderByDescending(g => g.count)
+                .Take(15)
+                .Select(g => g.Key)
                 .ToList();
         }
+        Console.WriteLine(model);
         return model;
     }
 }
 
 public class Area
 {
+    [JsonPropertyName("id")]
+    public required string Id { get; set; }
+
     [JsonPropertyName("name")]
     public required string Name { get; set; }
+
+    [JsonPropertyName("position")]
+    public int Position { get; set; }
+
     [JsonPropertyName("comments")]
-    public List<Comment>? Comments { get; set; }
+    public List<Comment> Comments { get; set; }
+
     [JsonIgnore]
-    public List<string>? Chips { get; set; }
+    public List<string> Tags { get; set; }
 }
 
 public class Comment
 {
-    [JsonPropertyName("key")]
-    public int Key { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Sections { GreatJob, ThinkAbout }
+
+    [JsonPropertyName("id")]
+    public required string Id { get; set; }
+
     [JsonPropertyName("text")]
     public required string Text { get; set; }
-    [JsonPropertyName("rating")]
-    public int Rating { get; set; }
+
+    [JsonPropertyName("position")]
+    public int Position { get; set; }
+
+    [JsonPropertyName("section")]
+    public Sections Section { get; set; }
+
+    [JsonIgnore]
+    public bool Selected { get; set; }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -8,13 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddSingleton(s =>
-{
-    using var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
-    return Model.GetModel(httpClient);
-});
-
+builder.Services.AddSingleton(s => Model.GetModel(builder.HostEnvironment.BaseAddress));
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
